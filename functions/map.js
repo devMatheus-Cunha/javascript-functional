4400755f2b4c5523c6a7cffc917c9ef3266e4fa4
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-03-16 18:17:37 -0300

    feat: two exemple map

--- functions/map.js
+++ functions/map.js
@@ -1,3 +1,4 @@
+// 1) Exemple one
 const nums = [1, 2, 3, 4, 5]
 const double = (n, i, a) => n * 2 + i + a[i]
 console.log(nums.map(double));
@@ -6,3 +7,40 @@ const names = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa']
 const firstWord = texto => texto[0]
 const words = names.map(firstWord)
 console.log(names, words)
+
+// 2) Exemple one
+const cart = [{
+    name: 'Caneta',
+    qtd: 10,
+    price: 7.99
+  },
+  {
+    name: 'Impressora',
+    qtd: 0,
+    price: 649.50
+  },
+  {
+    name: 'Caderno',
+    qtd: 4,
+    price: 27.10
+  },
+  {
+    name: 'Lapis',
+    qtd: 3,
+    price: 5.82
+  },
+  {
+    name: 'Tesoura',
+    qtd: 1,
+    price: 19.20
+  },
+]
+
+const getNamesCart = item => item.name
+
+const calcQtdXPrice = () => {
+  return cart.map(item => item.qtd * item.price)
+}
+
+console.log(cart.map(getNamesCart))
+console.log(calcQtdXPrice())
