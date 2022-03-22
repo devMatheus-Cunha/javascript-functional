c5357b74884dd3546a33ff3a1d26ab2e7e17b05b
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-03-22 12:12:02 -0300

    feat: more about promise

--- functions/promise.js
+++ functions/promise.js
@@ -1,3 +1,4 @@
+// 1)
 let newPromiseOne = new Promise((resolve) => {
   resolve(3)
 })
@@ -11,6 +12,8 @@ newPromiseTwo.then(value => value[0])
   .then(letter => letter.toLowerCase())
   .then(lowerCase => console.log(lowerCase))
 
+
+// 2)
 function awaitPromise(time = 2000) {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -20,3 +23,18 @@ function awaitPromise(time = 2000) {
   })
 }
 awaitPromise().then(() => awaitPromise(3000)).then(awaitPromise)
+
+// 3)
+function gerarNumerosEntre (min, max) {
+  if (min > max) {
+       [max, min] = [min, max] 
+ }
+  return new Promise(resolve => {
+       const fator = max - min + 1
+       const aleatorio = parseInt (Math.random () * fator) + min
+       resolve(aleatorio)
+ })
+}
+gerarNumerosEntre(1, 60)
+  .then(num => num * 10)
+  .then(numX10 => console.log(`O numero gerado foi ${numX10}`)) 
