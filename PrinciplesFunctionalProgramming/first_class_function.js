671ce9e9a77079e6f22d43274d158abc19f50d0e
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-08-01 17:40:16 +0100

    feat: firts class functions

---
+++ PrinciplesFunctionalProgramming/first_class_function.js
@@ -0,0 +1,9 @@
+//? A programming language is said to have first-class functions when functions in that language are treated like any other variable.
+
+const x = 3
+const y = (txt) => {
+  return `Hello my brother ${txt}`
+}
+
+console.log(x);
+console.log(y("Pedro"));
