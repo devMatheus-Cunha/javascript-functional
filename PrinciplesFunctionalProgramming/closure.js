dc8c75d84d1412b8ace12e545c1d418ab0efce52
Matheus Cunha <devmatheusgr@gmail.com>
2022-12-21 11:31:40 +0000

    feat: clousure

---
+++ PrinciplesFunctionalProgramming/closure.js
@@ -0,0 +1,5 @@
+// closure is when a function "remembers" its lexical scope, same when a function is exec outside this lexical scope
+
+const sumXMore3 = require("./closure_scope")
+
+console.log(sumXMore3())
---
+++ PrinciplesFunctionalProgramming/closure_scope.js
@@ -0,0 +1,7 @@
+const x = 3
+
+function sumXMore3() {
+ return x + 3
+}
+
+module.exports = sumXMore3
