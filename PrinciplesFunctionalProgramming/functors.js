ca3b7f58f9b9b60004498c0af0e83b539f3f9963
Matheus Cunha <devmatheusgr@gmail.com>
2022-12-28 11:27:11 +0000

    feat: functors

---
+++ PrinciplesFunctionalProgramming/functors.js
@@ -0,0 +1,29 @@
+// functors s'ao objetos que implementam a função MAP que também é um 'wrapper' de um valor 
+
+// Array e uum exemplo de FUNCTORS
+const nums = [1, 2, 3, 4, 5, 6]
+const newNums = nums.map(el => el + 10).map(el => el * 2)
+console.log(nums, newNums)
+
+function TypeSecutiry(value) {
+ return {
+  value,
+  invalidValue() {
+   return this.value === null || this.value === undefined
+  },
+  map(fn) {
+   if (this.invalidValue()) {
+    return TypeSecutiry(null)
+   } else {
+    const newValue = fn(this.value)
+    return TypeSecutiry(newValue)
+   }
+  }
+ }
+}
+
+const result = TypeSecutiry('This is a text')
+ .map(t => t.toUpperCase())
+ .map(t => t.split('').join(' '))
+
+console.log(result.value)
